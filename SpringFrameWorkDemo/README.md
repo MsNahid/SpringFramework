15e8e9e08c07864512d22130d88b8943f0ebc2d2
MsNahid <m.nahidzaman@gmail.com>
2022-05-11 15:13:58 +0600

    Added some notes

---
+++ SpringFrameWorkDemo/README.md
@@ -0,0 +1,13 @@
+# Spring Core
+## Spring Ioc container
+
+- Create Object
+- Manage Lifecycle Of Object
+- Inject Object at run time
+- BeanFactory Interface
+- ApplicationContext Interface
+- ClassPathXmlApplicationContext
+
+
+
+   
--- SpringFrameWorkDemo/src/main/java/com/nahidsohel/EmailApplication.java
+++ SpringFrameWorkDemo/src/main/java/com/nahidsohel/EmailApplication.java
@@ -1,10 +1,8 @@
 package com.nahidsohel;
 
 public class EmailApplication {
-
-    private BasicSpellChecker basicSpellChecker;
     public static void main(String[] args) {
-        EmailClient emailClient = new EmailClient(new BasicSpellChecker());
+        EmailClient emailClient = new EmailClient(new AdvancedSpellChecker());
         emailClient.sendMail("Hey, It's Nahid.This is my firstMail.");
         emailClient.sendMail("Hey, It's Tom. This is my secondMail.");
     }
