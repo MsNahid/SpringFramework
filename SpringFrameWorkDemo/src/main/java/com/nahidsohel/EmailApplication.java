6247402b54a3353905cb66e0b686f11d51e657c6
MsNahid <m.nahidzaman@gmail.com>
2022-05-12 13:03:50 +0600

    xml based configuration done.

--- SpringFrameWorkDemo/src/main/java/com/nahidsohel/EmailApplication.java
+++ SpringFrameWorkDemo/src/main/java/com/nahidsohel/EmailApplication.java
@@ -1,8 +1,14 @@
 package com.nahidsohel;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class EmailApplication {
     public static void main(String[] args) {
-        EmailClient emailClient = new EmailClient(new AdvancedSpellChecker());
+
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
+        EmailClient emailClient = applicationContext.getBean("emailClient", EmailClient.class);
+//        EmailClient emailClient = new EmailClient(new AdvancedSpellChecker());
         emailClient.sendMail("Hey, It's Nahid.This is my firstMail.");
         emailClient.sendMail("Hey, It's Tom. This is my secondMail.");
     }
